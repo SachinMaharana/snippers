a6b2abfec9b9cca6e3c2db0ba01d15e25a00d6e7
sachin maharana <sachin.nicky@gmail.com>
2020-04-17 23:18:36 +0530

    db setup with diesel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+#[macro_use]
+extern crate diesel;
+
 use actix_web::http::StatusCode;
 use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer, Responder};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
-// use serde_json;
 
 async fn healthz() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().status(StatusCode::OK).json("ok"))
@@ -34,21 +39,6 @@ async fn show_snippet(query: web::Query<ShowSnippet>) -> impl Responder {
             HttpResponse::from_error(error::ErrorBadRequest(format!("Unable to Parse ID: {}", e)))
         }
     }
-    // snippet_id.parse()
-    // match snippet_id.parse::<u8>() {
-    //     Ok(snippet_id) => {
-    //         if snippet_id > 1 {
-    //             HttpResponse::Ok()
-    //                 .status(StatusCode::OK)
-    //                 .body("Display a Snippet")
-    //         } else {
-    //             HttpResponse::from_error(error::ErrorBadRequest("ID is less than 1"))
-    //         }
-    //     }
-    //     Err(e) => {
-    //         HttpResponse::from_error(error::ErrorBadRequest(format!("Unable to Parse ID: {}", e)))
-    //     }
-    // }
 }
 
 async fn create_snippet() -> Result<HttpResponse, Error> {
@@ -59,8 +49,15 @@ async fn create_snippet() -> Result<HttpResponse, Error> {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix=trace,actix_web=trace,actix_test=trace");
+    std::env::set_var(
+        "RUST_LOG",
+        "actix=info,actix_web=info,actix_test=info,diesel=debug",
+    );
     env_logger::init();
+    dotenv().ok();
+
+    let connect_url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    // let manager = ConnectionManager::<Sql>
 
     let endpoint = "127.0.0.1:8080";
     println!("Server listening on: {}", endpoint);
